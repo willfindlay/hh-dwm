62dd16ef9cad4adaa5fb23c474074bc3a3a67d1e
HousedHorse <william@wfindlay.com>
2019-06-13 18:02:47 -0400

    better keybindings

--- config.h
+++ config.h
@@ -72,23 +72,23 @@ static const char *termcmdlastwin[]  = { "termite", "-d", "$(cat ~/.last_dir)" }
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
-    { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+    { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
     { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
     { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmdlastwin } },
-    { MODKEY,                       XK_b,      togglebar,      {0} },
+    //{ MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
     { MODKEY,                       XK_Up,     focusstack,     {.i = +1 } },
     { MODKEY,                       XK_Down,   focusstack,     {.i = -1 } },
-    { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-    { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+    //{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+    //{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
     { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
     { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
     { MODKEY,                       XK_Left,   setmfact,       {.f = -0.05} },
     { MODKEY,                       XK_Right,  setmfact,       {.f = +0.05} },
-    { MODKEY,                       XK_Return, zoom,           {0} },
+    { MODKEY,                       XK_f,      zoom,           {0} },
     { MODKEY,                       XK_Tab,    view,           {0} },
-    { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+    { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
     { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
     { MODKEY|ShiftMask,             XK_space,  setlayout,      {.v = &layouts[1]} },
     { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -111,7 +111,7 @@ static Key keys[] = {
     TAGKEYS(                        XK_7,                      6)
     TAGKEYS(                        XK_8,                      7)
     TAGKEYS(                        XK_9,                      8)
-    { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { MODKEY|ShiftMask,             XK_e,      quit,           {0} },
 };
 
 /* button definitions */
