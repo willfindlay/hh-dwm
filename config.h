0156e4b6b2d5b87e54526bf431fcc3eb0271bb13
HousedHorse <william@wfindlay.com>
2019-06-13 20:51:22 -0400

    added window rules for my apps and icons

--- config.h
+++ config.h
@@ -26,7 +26,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { " 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8", " 9" };
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -35,7 +36,9 @@ static const Rule rules[] = {
      */
     /* class      instance    title       tags mask     isfloating   monitor */
     { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-    //{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    { "Firefox",  NULL,       NULL,       1 << (6-1),   0,           -1 },
+    { "Thunderbird",  NULL,   NULL,       1 << (8-1),   0,           -1 },
+    { "Spotify",  NULL,       NULL,       1 << (9-1),   0,           -1 },
 };
 
 /* layout(s) */
