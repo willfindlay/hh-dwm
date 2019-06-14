1a2488aa16d5ac0980bd73513621f9f05f784392
HousedHorse <william@wfindlay.com>
2019-06-14 13:29:50 -0400

    update config

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "MesloLGSDZ Nerd Font Mono:size=14" };
+static const char dmenufont[]       = "MesloLGSDZ Nerd Font Mono:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -34,11 +34,11 @@ static const Rule rules[] = {
      *  WM_CLASS(STRING) = instance, class
      *  WM_NAME(STRING) = title
      */
-    /* class      instance    title       tags mask     isfloating   monitor */
-    { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-    { "Firefox",  NULL,       NULL,       1 << (6-1),   0,           -1 },
-    { "Thunderbird",  NULL,   NULL,       1 << (8-1),   0,           -1 },
-    { "Spotify",  NULL,       NULL,       1 << (9-1),   0,           -1 },
+    /* class                  instance    title       tags mask     isfloating   monitor */
+    { "Gimp",                 NULL,       NULL,       0,            1,           -1 },
+    { "VirtualBox Manager",   NULL,       NULL,       1 << (7-1),   0,           -1 },
+    { "Thunderbird",          NULL,       NULL,       1 << (8-1),   0,           -1 },
+    { "Spotify",              NULL,       NULL,       1 << (9-1),   0,           -1 },
 };
 
 /* layout(s) */
