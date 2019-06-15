d6df61115269d20c6423d12a458044d96f5e5098
HousedHorse <william@wfindlay.com>
2019-06-15 18:54:59 -0400

    no snap, changed color, fixed termite last dir spawn, disabled focus follows mouse

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "MesloLGS Nerd Font Mono:size=14" };
@@ -11,7 +11,7 @@ static const char col_gray1[]       = "#292d3e";
 static const char col_gray2[]       = "#000000"; /* border color unfocused windows */
 static const char col_gray3[]       = "#96b5b4";
 static const char col_gray4[]       = "#c0c5ce";
-static const char col_cyan[]        = "#660066"; /* border color focused windows and tags */
+static const char col_cyan[]        = "#4d94ff"; /* border color focused windows and tags */
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "termite", NULL };
-static const char *termcmdlastwin[]  = { "termite", "-d", "$(cat ~/.last_dir)" };
+static const char *termcmdlastwin[]  = { "termite", "-d", "$(cat ~/.last_dir)", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
